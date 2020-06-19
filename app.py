097f267d3f9786688c00aa47e249f0e2078ac983
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-19 18:44:05 -0400

    Added duplicate check

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for
 import json
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +12,14 @@ def home():
 def about():
     if request.method=='POST':
         url={}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                url=json.load(urls_file)
+
+        if request.form['name'] in url.keys():
+            return redirect(url_for('home'))
+
         url[request.form['name']]={'url':request.form['code']}
         with open('urls.json','w') as url_file:
             json.dump(url,url_file)
