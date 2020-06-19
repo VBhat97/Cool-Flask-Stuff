1c5cd726266833a584762d061338b8e2cfca3599
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-19 17:00:43 -0400

    Added saving to json file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def home():
 @app.route("/your-url",methods=['GET','POST'])
 def about():
     if request.method=='POST':
+        url={}
+        url[request.form['name']]={'url':request.form['code']}
+        with open('urls.json','w') as url_file:
+            json.dump(url,url_file)
         return render_template('url.html',code=request.form['code'])
     else:
-        return "This is not allowed"
+        return redirect(url_for('home'))
