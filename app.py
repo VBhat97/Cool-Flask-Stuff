887e1b9703b652c3b306fefda9ffe5a36009d71c
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-18 21:59:27 -0400

    Added form and passed data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('home.html',name='Vaibhav')
 
-@app.route("/about")
+@app.route("/your-url")
 def about():
-    return 'About'
+    return render_template('url.html',code=request.args['code'])
