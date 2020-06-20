9d3267df1dbb7b092efa926fcca7e27bbbc51dc7
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-19 23:00:01 -0400

    Added file uploads

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+from flask import Flask,render_template,request,redirect,url_for,flash,abort
 import json
 import os
 from werkzeug.utils import secure_filename
@@ -23,13 +23,13 @@ def about():
             flash('The name is already taken, sorry. Try another one.')
             return redirect(url_for('home'))
 
-        if 'url' in request.form.keys():
+        if 'code' in request.form.keys():
             url[request.form['name']]={'url':request.form['code']}
         else:
             f=request.files['file']
             full_name=request.form['name'] + secure_filename(f.filename)
-            f.save('C:/Users/V.Bhat/Desktop/Graduate Studies/Co-Corriculars/Projects/Cool-Flask-Stuff/'+full_name)
-            url[request.form['name']]={'url':full_name}
+            f.save('C:/Users/V.Bhat/Desktop/Graduate Studies/Co-Corriculars/Projects/Cool-Flask-Stuff/static/user_files'+full_name)
+            url[request.form['name']]={'file':full_name}
 
         with open('urls.json','w') as url_file:
             json.dump(url,url_file)
@@ -44,4 +44,7 @@ def redirect_to_url(code):
             url=json.load(urls_file)
             if 'url' in url[code].keys():
                 return redirect(url[code]['url'])
+            else:
+                return redirect(url_for('static',filename='user_files'+url[code]['file']))
+    return abort(404)
     
