b1e9fa6b0a055434e0f6aa55706e852bd18a7b1d
VBhat97 <vaibhav.bhat2097@gmail.com>
2021-02-12 22:21:12 -0500

    Multiple inheritance

--- OOP in Python/test.py
+++ OOP in Python/test.py
@@ -1,17 +1,29 @@
-class Book :
-    def __init__(self,title,author,pages,price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
+# class Book :
+#     def __init__(self,title,author,pages,price):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+#         self.price = price
+#
+# class LightBook(Book):
+#     def __init__(self, title, author, pages, price, backcover):
+#         super().__init__(title,author,pages,price)
+#         self.backcover = backcover
 
-class LightBook(Book):
-    def __init__(self, title, author, pages, price, backcover):
-        super().__init__(title,author,pages,price)
-        self.backcover = backcover
+class A:
+    def __init__(self):
+        self.name = "A"
 
+class B:
+    def __init__(self):
+        self.name = "B"
 
-L1 = LightBook("Small things","John Reynold",321,29.95,"cardboard")
-B1 = Book("Small things","John Reynold",321,29.95)
+class C(B,A):
+    def __init__(self):
+        super().__init__()
 
-print(L1.backcover)
+    def giveName(self):
+        print(self.name)
+
+c1 = C()
+print(c1.giveName())
