bc5ae41e3edf6dfcdc54a01452c2ad38fa7b9b63
VBhat97 <vaibhav.bhat2097@gmail.com>
2021-02-13 04:26:08 -0500

    Python Magic methods in OOP

--- OOP in Python/test.py
+++ OOP in Python/test.py
@@ -9,21 +9,36 @@
 #     def __init__(self, title, author, pages, price, backcover):
 #         super().__init__(title,author,pages,price)
 #         self.backcover = backcover
+#
+# class A:
+#     def __init__(self):
+#         self.name = "A"
+#
+# class B:
+#     def __init__(self):
+#         self.name = "B"
+#
+# class C(B,A):
+#     def __init__(self):
+#         super().__init__()
+#
+#     def giveName(self):
+#         print(self.name)
+#
+# c1 = C()
+# print(c1.giveName())
 
-class A:
-    def __init__(self):
-        self.name = "A"
+#Magic Methods
 
-class B:
-    def __init__(self):
-        self.name = "B"
+class Book:
+    def __init__(self,title,author,price):
+        self.title = title
+        self.author = author
+        self.price = price
 
-class C(B,A):
-    def __init__(self):
-        super().__init__()
+    def __str__(self):
+        return f"{self.title} written by {self.author}, costs {self.price}"
 
-    def giveName(self):
-        print(self.name)
+b1=Book("Angels and Demons","Dan Brown","29.95")
 
-c1 = C()
-print(c1.giveName())
+print(b1)
