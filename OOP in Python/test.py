9b7f1707b1c4e61f270ff285ca5c12276485f614
VBhat97 <vaibhav.bhat2097@gmail.com>
2021-02-12 21:30:18 -0500

    Inheritance in Python.

--- OOP in Python/test.py
+++ OOP in Python/test.py
@@ -1,1 +1,17 @@
-print("Hello World")
+class Book :
+    def __init__(self,title,author,pages,price):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.price = price
+
+class LightBook(Book):
+    def __init__(self, title, author, pages, price, backcover):
+        super().__init__(title,author,pages,price)
+        self.backcover = backcover
+
+
+L1 = LightBook("Small things","John Reynold",321,29.95,"cardboard")
+B1 = Book("Small things","John Reynold",321,29.95)
+
+print(L1.backcover)
